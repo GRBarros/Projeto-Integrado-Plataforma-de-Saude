b820e39fdea180f169efec4b70e51d65c6664610
GRBarros <gabrielr.barros@outlook.com>
2025-09-01 15:05:29 -0300

    Função de Cadastro de Paciente
    Finalizado a função para cadastro de pacientes. O cadastro foi realizado com listas e dicionário conforme solicitado no roteiro do projeto.
    
    Adicionei uma saída para cada função, para conseguir realizar os testes.
    
    Após o teste, todas as funções do menu está funcionando normalmente.

--- estrutura/main.py
+++ estrutura/main.py
@@ -7,50 +7,61 @@ lista_cadastro = list()
 
 # Opção 1: Cadastrar pacientes.
 def cadastro():
-
+    print("==== CADASTRO DE PACIENTE ====")
+    paciente['nome'] = str(input("Nome do paciente: ")).capitalize()
+    paciente['idade'] = int(input("Idade: "))
+    paciente['Telefone'] = input("Telefone: ")
+    lista_cadastro.append(paciente.copy())
+    
+    print("\nCADASTRO REALIZADO COM SUCESSO!\n")
+    
 # Opção 2: Ver estatísticas
 def estatiscicas():
+    print("Estatísticas")
     
 # Opção 3: Buscar paciente
 def buscar():
+    print("Buscar")
 
 #opção 4: Listar todos os pacientes
 def lista():
+    print(lista_cadastro)
 
 def sair():
     exit()
     
 # Menu 
 def menu():
-    print("==== SISTEMA CLÍNICA VIDA+ ====")
-    opcao = int(input("1. Cadastrar paciente\n"
-                      "2. Ver estatísticas\n"
-                      "3. Buscar paciente\n"
-                      "4. Listar todos os pacientes\n"
-                      "5. Sair\n"
-                      "Escolha uma opção: "))
-    print("="*31,
-          "\n")
-    
-    # Estrutura condicional do menu
-    if opcao == 1: # Opção para cadastro de pacientes
-        cadastro()
-    
-    elif opcao == 2: # Opção para visualizar as estatísticas
-        estatiscicas()
+    while True:
+        print("==== SISTEMA CLÍNICA VIDA+ ====")
+        opcao = int(input("1. Cadastrar paciente\n"
+                        "2. Ver estatísticas\n"
+                        "3. Buscar paciente\n"
+                        "4. Listar todos os pacientes\n"
+                        "5. Sair\n"
+                        "Escolha uma opção: "))
+        print("="*31,
+            "\n")
         
-    elif opcao == 3: # Opção para pesquisar paciente
-        buscar()
-    
-    elif opcao == 4: # Opção para listar pacientes 
-        lista()
-    
-    elif opcao == 5: # Opção para sair
-        sair()
+        # Estrutura condicional do menu
+        if opcao == 1: # Opção para cadastro de pacientes
+            cadastro()
         
-    else:
-        print("Opção inválida! Tente novamente.\n")
-        menu()
+        elif opcao == 2: # Opção para visualizar as estatísticas
+            estatiscicas()
+            
+        elif opcao == 3: # Opção para pesquisar paciente
+            buscar()
+        
+        elif opcao == 4: # Opção para listar pacientes 
+            lista()
+        
+        elif opcao == 5: # Opção para sair
+            sair()
+            
+        else:
+            print("Opção inválida! Tente novamente.\n")
+            menu()
         
 if __name__ == "__main__":
     menu()
