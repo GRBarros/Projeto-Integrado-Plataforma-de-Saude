7d47a5932944dfd98dc7adb6125ef664035ee7b8
GRBarros <gabrielr.barros@outlook.com>
2025-09-01 14:38:45 -0300

    Estrutura das Funções
    Neste commit, estruturei como será as funções das opções do menu, e também realizei a adição da estrutura condicional  para cada opção do menu

--- estrutura/main.py
+++ estrutura/main.py
@@ -6,18 +6,20 @@ lista_cadastro = list()
 
 
 # Opção 1: Cadastrar pacientes.
-#def cadastro():
+def cadastro():
 
 # Opção 2: Ver estatísticas
-#def estatiscicas():
+def estatiscicas():
     
 # Opção 3: Buscar paciente
-#def buscar():
+def buscar():
 
 #opção 4: Listar todos os pacientes
-#def Lista():
+def lista():
 
-
+def sair():
+    exit()
+    
 # Menu 
 def menu():
     print("==== SISTEMA CLÍNICA VIDA+ ====")
@@ -30,6 +32,26 @@ def menu():
     print("="*31,
           "\n")
     
+    # Estrutura condicional do menu
+    if opcao == 1: # Opção para cadastro de pacientes
+        cadastro()
+    
+    elif opcao == 2: # Opção para visualizar as estatísticas
+        estatiscicas()
+        
+    elif opcao == 3: # Opção para pesquisar paciente
+        buscar()
+    
+    elif opcao == 4: # Opção para listar pacientes 
+        lista()
+    
+    elif opcao == 5: # Opção para sair
+        sair()
+        
+    else:
+        print("Opção inválida! Tente novamente.\n")
+        menu()
+        
 if __name__ == "__main__":
     menu()
     
